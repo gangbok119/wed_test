9687f2bb92b681cd5fcd7d05bf7cb8f998fb2415
Kangbok Lee <gangbok119@gmail.com>
2017-10-18 11:06:09 +0900

    post_detail 구현

--- wed_test/post/urls.py
+++ wed_test/post/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import post_list
+from .views import post_list, post_detail
 
 urlpatterns = [
     url(r'post/$', post_list, name='post_list'),
+    url(r'post/(?P<pk>\d+)/$',post_detail, name='post_detail'),
 ]
