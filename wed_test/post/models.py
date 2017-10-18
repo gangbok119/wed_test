b7d57acaecc10c2f2e9d7e0e31dcd7af36a891d0
Kangbok Lee <gangbok119@gmail.com>
2017-10-18 10:25:57 +0900

    post model 설계

--- wed_test/post/models.py
+++ wed_test/post/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.TextField()
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
