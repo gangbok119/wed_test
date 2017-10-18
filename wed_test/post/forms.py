7516bfe8712d1f55b89589c466a99fd50a5be7a9
Kangbok Lee <gangbok119@gmail.com>
2017-10-18 11:52:57 +0900

    post create 기능 구현

--- wed_test/post/forms.py
+++ wed_test/post/forms.py
@@ -5,7 +5,7 @@ from .models import Post
 
 class PostForm(forms.Form):
 
-    class Meta():
-        model= Post()
+    class Meta:
+        model= Post
         fields = ('title', 'text',)
 
--- wed_test/post/views.py
+++ wed_test/post/views.py
@@ -26,15 +26,13 @@ def post_detail(request,pk):
 
 def post_create(request):
     if request.method=='POST':
-        form = PostForm(request.POST)
-        Post.objects.create(title=form.title,text=form.text)
+        title =request.POST['title']
+        text = request.POST['text']
+        user = User.objects.first()
+        Post.objects.create(title=title,text=text,author=user)
         return redirect('post_list')
     else:
-        form =PostForm()
-        context = {
-            'form':form
-        }
-        return render(request,'post/post_create.html',context)
+        return render(request,'post/post_create.html',)
 
 def post_delete(request,pk):
     if request.method =="POST":
